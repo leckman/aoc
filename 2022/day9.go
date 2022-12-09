60b9a6ead4835a35c134335d6cbfa764a1ea459f
Laura Eckman <laura@factual.com>
2022-12-09 15:22:19 -0500

    Update day9.go

--- 2022/day9.go
+++ 2022/day9.go
@@ -21,9 +21,7 @@ func part1(directions []string) {
 		direction, steps := parseDirective(l)
 		for s := 0; s < steps; s++ {
 			head = moveHead(direction, head)
-			if !isAdjacent(head, tail) {
-				tail = moveTail(head, tail)
-			}
+			tail = moveTail(head, tail)
 			visited[positionToKey(tail)] = struct{}{}
 		}
 	}
@@ -32,7 +30,7 @@ func part1(directions []string) {
 }
 
 func part2(directions []string) {
-	fmt.Println("Part 1:")
+	fmt.Println("Part 2:")
 	knots := make([][]int, 10)
 	for k := range knots {
 		knots[k] = []int{0, 0}
@@ -44,9 +42,7 @@ func part2(directions []string) {
 		for s := 0; s < steps; s++ {
 			knots[0] = moveHead(direction, knots[0])
 			for k := 1; k < len(knots); k++ {
-				if !isAdjacent(knots[k-1], knots[k]) {
-					knots[k] = moveTail(knots[k-1], knots[k])
-				}
+				knots[k] = moveTail(knots[k-1], knots[k])
 			}
 			tail := knots[len(knots)-1]
 			visited[positionToKey(tail)] = struct{}{}
@@ -63,70 +59,63 @@ func isAdjacent(head []int, tail []int) bool {
 	return withinOneSpace(head[0], tail[0]) && withinOneSpace(head[1], tail[1])
 }
 
-func moveTail(head []int, tail []int) []int {
-	below := head[0] + 1
-	above := head[0] - 1
-	left := head[1] - 1
-	right := head[1] + 1
+func moveUp(position []int) []int {
+	position[0] -= 1
+	return position
+}
 
-	isVerticallyClose := withinOneSpace(head[0], tail[0])
-	isBelow := tail[0] > head[0]
-	if tail[1] < left {
-		// D
-		// T d . .
-		// T t H .
-		// T d . .
-		// D
-		if isVerticallyClose {
-			return []int{head[0], left}
-		} else if isBelow {
-			return []int{below, left}
-		} else {
-			return []int{above, left}
-		}
+func moveDown(position []int) []int {
+	position[0] += 1
+	return position
+}
+
+func moveLeft(position []int) []int {
+	position[1] -= 1
+	return position
+}
+
+func moveRight(position []int) []int {
+	position[1] += 1
+	return position
+}
+
+func moveTail(head []int, tail []int) []int {
+	if isAdjacent(head, tail) {
+		return tail
 	}
-	if tail[1] > right {
-		//        D
-		//  . . d T
-		//  . H t T
-		//  . . d T
-		//        D
-		if isVerticallyClose {
-			return []int{head[0], right}
-		} else if isBelow {
-			return []int{below, right}
-		} else {
-			return []int{above, right}
-		}
+	// Below
+	if tail[0] > head[0] {
+		tail = moveUp(tail)
 	}
-	if isBelow {
-		//  . H .
-		//  . t .
-		//  T T T
-		return []int{below, head[1]}
+	// Above
+	if tail[0] < head[0] {
+		tail = moveDown(tail)
 	}
-	// Leftover case: above & horizontally close
-	//  T T T
-	//  . t .
-	//  . H .
-	return []int{above, head[1]}
+	// Left
+	if tail[1] < head[1] {
+		tail = moveRight(tail)
+	}
+	// Right
+	if tail[1] > head[1] {
+		tail = moveLeft(tail)
+	}
+	return tail
 }
 
 func moveHead(direction string, head []int) []int {
 	switch direction {
 	case "D":
-		head[0] += 1
+		return moveDown(head)
 	case "U":
-		head[0] -= 1
+		return moveUp(head)
 	case "L":
-		head[1] -= 1
+		return moveLeft(head)
 	case "R":
-		head[1] += 1
+		return moveRight(head)
 	default:
 		fmt.Println(direction)
 		panic("Unknown direction")
 	}
-	return head
 }
 
 func parseDirective(l string) (string, int) {
